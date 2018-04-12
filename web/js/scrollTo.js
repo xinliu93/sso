8c346e1b901ea76d5076c64e388aa4e9d8fbc472
xinliu93 <webdeveloperlx@163.com>
2018-04-12 13:59:27 +0800

    Add files via upload

---
+++ web/js/scrollTo.js
@@ -0,0 +1,30 @@
+
+(function($){
+	$.extend($.fn,{
+		scrollTo:function(time,to){
+			time=time||800;
+			to=to||1;
+            $('a[href*=#]', this).click(function(){
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                    var $target = $(this.hash);
+                    $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+                    if ($target.length) {
+                        if (to == 1) {
+                            $('html,body').animate({
+                                scrollTop: $target.offset().top
+                            }, time);
+                        }
+                        else if(to==2){
+                            $('html,body').animate({
+                                scrollLeft: $target.offset().left
+                            }, time);
+                        }else{
+							alert('argument error！');
+						}
+                        return false;
+                    }
+                }
+            });
+		}
+	});
+})(jQuery);
